92a634d3facc230a9b601bb9c01587bba855f151
Jogi Nayak <jogi@aisthetic.co>
2025-04-22 16:43:13 +0530

    install analytics

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import type { AppProps } from 'next/app'
+import Script from 'next/script'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      {/* Google Analytics Script */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-5SREHPDN1E"
+      ></Script>
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-5SREHPDN1E');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
+}
