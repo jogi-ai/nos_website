c94354aaee1ccb240b2ceeb613c9354d6591bd9a
Jogi Nayak <jogi@aisthetic.co>
2025-04-22 16:53:46 +0530

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,17 +5,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       {/* Google Analytics Script */}
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-5SREHPDN1E"
-      ></Script>
-      <Script id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-5SREHPDN1E');
-        `}
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-5SREHPDN1E"></Script>
+      <Script dangerouslySetInnerHTML={{__html:`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-5SREHPDN1E', { debug_mode: ${process.env.NEXT_PUBLIC_ENV=="production"?"false":"true"} });
+      `}}>
       </Script>
       <Component {...pageProps} />
     </>
